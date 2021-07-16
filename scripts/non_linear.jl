b01cdd9a6c298cff1da4528f3ea436b18675d376
Carson Farmer <59753859+cfarm6@users.noreply.github.com>
2021-07-16 17:43:05 -0400

    Adding Underlying Graph Structure

--- scripts/non_linear.jl
+++ scripts/non_linear.jl
@@ -27,26 +27,7 @@ add_1J!(msd, Dict(
     :I => true,
     :C => true
     ), :J1_1)
-##
+## Plot Underlying Graph
 name = msd.graph.vprops
 name = map(x -> "\$" * string(name[x][:name]) * "\$", 1:length(values(msd.graph.vprops)))
 plt = TikzGraphs.plot(msd.graph.graph, name) |> display
-TikzGraphs.plot(msd.graph.graph)
-##
-num_vertices = length(msd.elements) + length(msd.junctions)
-g = SimpleGraph(num_vertices)
-mg = MetaGraph(g)
-set_indexing_prop!(mg, :name)
-element_names = collect(keys(msd.elements))
-junction_names = collect(keys(msd.junctions))
-all_names = [element_names; junction_names]
-for i  ∈ eachindex(all_names)
-    set_prop!(mg,  i, :name,  all_names[i])
-end
-for i ∈ keys(msd.junctions)
-    for j ∈ keys(msd.junctions[i].elements)
-        add_edge!(mg, mg[i, :name], mg[j, :name])
-    end
-end
-plt = TikzGraphs.plot(mg.graph, "\$" .* string.(all_names) .* "\$")
-TikzPictures.save(TEX(plotsdir("graph")), plt)
