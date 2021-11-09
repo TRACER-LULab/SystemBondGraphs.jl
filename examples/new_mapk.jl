0885b142de04e98436b3d071348a69a01c931871
Carson Farmer <cfarmer6@liberty.edu>
2021-11-09 20:52:03 +0000

    Working equation generation for MAPK Cascade

--- examples/new_mapk.jl
+++ examples/new_mapk.jl
@@ -32,10 +32,11 @@ function swap_defaults(BG, name, old, new)
 end
 ## Swap element for a bond element
 function swap_bond(BG, elem)
-    name = S(elem)
-    add_Bond!(BG, :temp)
-    swap(BG, elem, "temp")
-    set_prop!(BG.graph, BG.graph[:temp, :name], :name, name)
+    old_name = S(elem)
+    new_name = S(elem*"_bond")
+    add_Bond!(BG, new_name)
+    swap(BG, elem, string(new_name))
+    set_prop!(BG.graph, BG.graph[new_name, :name], :name, old_name)
 end
 ## Kinase Factory
 function kinase_factory!(BG, name; KM=1.0, KE=1.0)
@@ -48,24 +49,24 @@ function kinase_factory!(BG, name; KM=1.0, KE=1.0)
     for i ∈ 1:6
         add_Bond!(BG, S(name*"B$(i)"))
     end
-    add_0J!(BG, Dict(
+    add_1J!(BG, Dict(
         S(name*"B1") => true,
         S(name*"B2") => false,
         S(name*"ATP") => true,
         S(name*"M") => true
         ), S(name*"J_M"))
-    add_0J!(BG, Dict(
+    add_1J!(BG, Dict(
         S(name*"B5") => true,
         S(name*"MP") => false,
         S(name*"ADP") => false,
         S(name*"B6") => false
         ), S(name*"J_MP"))
-    add_1J!(BG, Dict(
+    add_0J!(BG, Dict(
         S(name*"B3") => true, 
         S(name*"B4") => false,
         S(name*"C") => false
         ), S(name*"J_C"))
-    add_1J!(BG, Dict(
+    add_0J!(BG, Dict(
         S(name*"B6") => true,
         S(name*"E") => false,
         S(name*"B1") => false
@@ -103,26 +104,26 @@ function phosphatase_factory!(BG, name; KMP=1.0, KE=1.0)
     for i ∈ 1:6
         add_Bond!(BG, S(name*"B$(i)"))
     end
-    add_0J!(BG, Dict(
+    add_1J!(BG, Dict(
         S(name*"B1") => true,
         S(name*"B2") => false,
         S(name*"MP") => true
         ), S(name*"J_MP"))
-    add_0J!(BG, Dict(
+    add_1J!(BG, Dict(
         S(name*"B5") => true,
         S(name*"B6") => false,
         S(name*"M") => false,
         S(name*"P") => false
         ), S(name*"J_M"))
-    add_1J!(BG, Dict(
+    add_0J!(BG, Dict(
         S(name*"B3") => true,
         S(name*"C") => false,
         S(name*"B4") => false
         ), S(name*"J_C"))
-    add_1J!(BG, Dict(
+    add_0J!(BG, Dict(
         S(name*"B6") => true,
         S(name*"B1") => false,
-        S(name*"E") => true
+        S(name*"E") => false
         ), S(name*"J_E"))
     add_Re!(BG, S(name*"B2"), S(name*"B3"), S(name*"Re1"))
     add_Re!(BG, S(name*"B4"), S(name*"B5"), S(name*"Re2"))
@@ -170,6 +171,11 @@ function phosphorylation_cycle_factory!(BG, name;KM=1.0, KMP=1.0, KKin=1.0)
         S(name*"K_MP") => true,
         S(name*"P_MP") => false
         ), S(name*"J_MP"))
+    # Swap Species inputs for bond elements
+    swap_bond(BG, name*"K_M")
+    swap_bond(BG, name*"K_MP")
+    swap_bond(BG, name*"P_MP")
+    swap_bond(BG, name*"P_M")
     # Swap Components to create Cycle Ports
     swap(BG, name*"K_ATP", name*"ATP")
     swap(BG, name*"K_ADP", name*"ADP")
@@ -249,13 +255,18 @@ function mapk_cascade_factory!(BG, name)
             ]
     for c ∈ phosphorylation_cycles
         phosphorylation_cycle_factory!(BG, c[:c]; KM=Kdict[c[:s]], KMP=Kdict[c[:p]], KKin=Kdict[c[:k]])
-        swap_bond(BG, S(name*c[:c]*"Kin"))
-        swap_bond(BG, S(name*c[:c]*"Pho"))
-        swap_bond(BG, S(name*c[:c]*"M"))
-        swap_bond(BG, S(name*c[:c]*"MP"))
-        swap_bond(BG, S(name*c[:c]*"ATP"))
-        swap_bond(BG, S(name*c[:c]*"ADP"))
-        swap_bond(BG, S(name*c[:c]*"P"))
+        if c[:c] == "cycle1_"
+            for chemostat ∈ chemostats
+                swap_defaults(BG, name, c[:c]*chemostat, chemostat)
+            end
+        end
+        swap_bond(BG, name*c[:c]*"Kin")
+        swap_bond(BG, name*c[:c]*"Pho")
+        swap_bond(BG, name*c[:c]*"M")
+        swap_bond(BG, name*c[:c]*"MP")
+        swap_bond(BG, name*c[:c]*"ATP")
+        swap_bond(BG, name*c[:c]*"ADP")
+        swap_bond(BG, name*c[:c]*"P")
         species_dict[c[:k]][S(name*c[:c]*"Kin")]   = !true
         species_dict[c[:ph]][S(name*c[:c]*"Pho")]  = !true
         species_dict[c[:s]][S(name*c[:c]*"M")]     = !true
@@ -265,11 +276,10 @@ function mapk_cascade_factory!(BG, name)
         chemostat_dict["P"][S(name*c[:c]*"P")]     = !true
     end
     for s ∈ species
-        display(species_dict[s])
+        BG[S(name*s)].k = Kdict[s]
         add_0J!(BG, species_dict[s], S(name*"J_"*s))
     end
     for c ∈ chemostats 
-        display(chemostat_dict[c])
         add_0J!(BG, chemostat_dict[c], S(name*"J_"*c))
     end
 end
@@ -278,11 +288,40 @@ end
 test = BondGraph(t) 
 phosphorylation_cycle_factory!(test, "")
 model = generate_model(test)
+##
 test = BondGraph(t)
 mapk_cascade_factory!(test, "")
 model = generate_model(test)
+model = structural_simplify(model)
+##
+u0 = [
+    test[:MKKKK].q      => 3e-5,
+    test[:MKKK].q       => 3e-3,
+    test[:MKK].q        => 1.2,
+    test[:MK].q         => 1.2,
+    test[:MAPKKKPase].q => 3e-4,
+    test[:MAPKKPase].q  => 3e-4,
+    test[:MAPKPase].q   => 1.2e-1
+]
+tspan = (0.0, 0.1)
+prob = ODEProblem(model, u0, tspan, [])
+sol = solve(prob)
 ## Plotting
 import Pkg; Pkg.activate()
 nodenames = map(i->string(test.graph.vprops[i][:name]), 1:nv(test.graph))
 layout=(args...)->spring_layout(args...; C=70)
 gplot(test.graph, nodelabel = nodenames, layout = layout, linetype="curve")
+## 
+using Symbolics, SymbolicUtils
+##
+explog = @rule exp(log(~x))=>~x
+loglog = @rule 
+@variables x_0, x_1, x_2, x_12
+@rule(log(~x)+log(~y) => log(~x*~y))(log(x_2) + log(x_0))
+##
+file = open("equations.tex", "w") 
+eqns = equations(model)
+for i ∈ eachindex(eqns)
+    write(file, latexify(eqns[i]))
+end
+close(file)
