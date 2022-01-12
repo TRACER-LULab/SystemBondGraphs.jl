097a4eac13f894d3cfb13f7bb55892f218399c8c
Carson Farmer <carsonfarmer@icloud.com>
2022-01-12 10:46:56 -0500

    chua

--- examples/Chua_Oscillator.jl
+++ examples/Chua_Oscillator.jl
@@ -6,38 +6,45 @@ using DifferentialEquations
 chua = BondGraph(t)
 # Add Nonlinear element
 @parameters Ga Gb E
-Φr(e, f, t, p) = p[2] * e + 1 / 2 * (p[2] - p[1]) * (abs(f + p[3]) - abs(f - p[3]))
-add_R!(chua, Φr, [Ga, Gb, E], :R_nonlinear)
+Φr(e, f, t, p) = p[2] * e + 1 / 2 * (p[2] - p[1]) * (abs(e + p[3]) - abs(e - p[3]))
+add_R!(chua, :f => Φr, [Ga, Gb, E], :Gn)
 # Add Linear Elements
+add_C!(chua, :C1)
 add_C!(chua, :C2)
 add_R!(chua, :R)
-add_C!(chua, :C1)
+add_R!(chua, :G)
 add_I!(chua, :L)
 # Add Bonds
+add_Bond!(chua, :b2)
 add_Bond!(chua, :b3)
 add_Bond!(chua, :b5)
-add_Bond!(chua, :b7)
+add_Bond!(chua, :b8)
 # Add Junctions
+add_1J!(chua, Dict(
+  :C2 => false,
+  :b2 => false
+  ), :J11)
+add_1J!(chua, Dict(
+  :G => false,
+  :b3 => false
+  ), :J12)
 add_0J!(chua, Dict(
-    :R_nonlinear => false,
-    :C1 => false,
-    :b3 => true
+  :b2 => true,
+  :b3 => true,
+  :b5 => false
   ), :J01)
 add_1J!(chua, Dict(
-    :b3 => false,
-    :R => false,
-    :b5 => true
-  ), :J11)
-add_0J!(chua, Dict(
-    :b5 => false,
-    :C2 => false,
-    :b7 => true
-  ), :J02)
-add_1J!(chua, Dict(
-    :b7 => false,
-    :L => false
+  :b5 => true,
+  :L  => false,
+  :R  => false,
+  :b8 => false
   ), :J13)
-# Generate Model
+add_1J!(chua, Dict(
+  :b8 => true, 
+  :C1 => false,
+  :Gn => false
+), :J14)
+## Generate Model
 sys = generate_model(chua)
 sys = structural_simplify(sys)
 # states(sys)
